90d0be49d969cfe4146a95069db17126a496d302
Edward? <108749515+mebronquillo@users.noreply.github.com>
2025-07-01 21:41:33 +0800

    https://github.com/mebronquillo/Web_project.git

--- script.js
+++ script.js
@@ -19,16 +19,24 @@ function closeNav1() {
   }
 
 
-//Swiper Gallery//
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
